04dd4a38c2d4febe9a4513a4d1654cc826134b7e
danilovlad <danilo.vladicic@gmail.com>
2021-01-16 18:18:34 -0500

    Routes

--- server/app.js
+++ server/app.js
@@ -1,7 +1,28 @@
 const express = require('express');
 const app = express();
+const users = require('./data/uesrs.json')
+const recuriter = require('./data/recruiter.json');
+const jobs = require('./data/jobs.json');
+const matches = require('./data/matches.json');
+
 
 app.use(express.static('../client/htn-app/build'));
+
+app.get('/users', (req, res) => {
+    res.json(users)
+});
+
+app.get('/recruiter', (req, res) => {
+    res.json(recuriter)
+});
+
+app.get('/matches', (req, res) => {
+    res.json(matches)
+});
+
+app.get('/jobs', (req, res) => {
+    res.json(jobs)
+});
 
 app.get('/', (req, res) => {
     res.sendFile('../client/htn-app/build/index.html')
