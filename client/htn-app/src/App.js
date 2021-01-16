a1965950d35ef31e08c51e4dd4a9ca456459b5da
imilisav <mili2970@mylaurier.ca>
2021-01-16 18:06:10 -0500

    Add Routes

--- client/htn-app/src/App.js
+++ client/htn-app/src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Switch>
+          <Route path="/"></Route>
+          <Route path="/login"></Route>
+          <Route path="/register"></Route>
+          <Route path="/profile"></Route>
+          <Route path="/recruiter"></Route>
+          <Route path="/matches"></Route>
+          <Route path="/swipe"></Route>
+          <Route path="/chat/:id"></Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
